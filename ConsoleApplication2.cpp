3d48fab28ac7fe6c2946f6c099ba942f50066c66
Dmitriy148 <isip_d.d.timofeev@mpt.ru>
2019-11-28 16:50:33 +0300

    lol

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,17 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+
+
+int main()
+{
+	using namespace std;
+	double c = 0, d = 0, a = 0;
+	setlocale(LC_ALL, "");
+	cout << "Введите числа";
+	cin >> c;
+	cin >> d;
+	cin >> a;
+	cout << (2 * c +log10(d) * 51) / (d - a - 1);
+	return 0;
+}
